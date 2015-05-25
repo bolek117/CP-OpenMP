e54d04d7490f61e4ed1b547fe436a44c428547bd
mwitas <bolek117@interia.pl>
2015-05-25 15:29:36 +0200

    Task 1, part 1 working

---
+++ cp_omp/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+long num_steps = 100000;
+
+double pi_sequential()
+{
+	double x, sum = 0.0;
+	double step = 1.0 / (double)num_steps;
+
+	for (long i = 1; i <= num_steps; i++) 
+	{
+		x = (i - 0.5) * step;
+		sum += 4.0 / (1.0 + pow(x, 2.0));
+	}
+
+	//cout << "Sequential sum = " << sum << '\n';
+	double pi = step * sum;
+	return pi;
+}
+
+double pi_parallel(const unsigned int noOfThreads)
+{
+	const double step = 1.0 / (double)num_steps;
+
+	double *buffer;
+	buffer = (double*)malloc(noOfThreads * sizeof(double));
+	for (int i = 0; i < noOfThreads; i++)
+	{
+		buffer[i] = 0.0;
+	}
+
+	#pragma omp parallel num_threads (noOfThreads)
+	{
+		double x;
+		const int thread_num = omp_get_thread_num();
+		double increment = 0.0;
+
+		for (long i = thread_num + 1; i <= num_steps; i += noOfThreads)
+		{
+			x = (i - 0.5) * step;
+			increment += 4.0 / (1.0 + pow(x, 2.0));
+		}
+
+		buffer[thread_num] = increment;
+		//printf("Thread %d = %f\n", thread_num, increment);
+	}
+
+	double sum = 0.0;
+	for (int i = 0; i < noOfThreads; i++)
+		sum += buffer[i];
+
+	//printf("Concurent sum = %f\n", sum);
+	double pi = step * sum;
+
+	free(buffer);
+	return pi;
+}
+
+int main() 
+{
+	cout << "Sequential = " << pi_sequential() << '\n';
+	cout << "Parallel = " << pi_parallel(4) << '\n';
+	
+	char x;
+	cin >> x;
+	return 0;
+}
